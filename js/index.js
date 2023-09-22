fcd2ace82a366964c1d3a57e49c9c5699a819e48
Sunny-Siddique <rabbysanny088@gmail.com>
2023-09-22 15:02:38 +0600

    html css and javascript change

---
+++ js/index.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".main-slider-box", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".st-generosity",
+    prevEl: ".st-avarice ",
+  },
+  breakpoints:{
+    0:{
+      slidesPerView: 1,
+    },
+   520:{
+      slidesPerView: 2,
+    },
+    950:{
+      slidesPerView: 3,
+    },
+  },
+});
+
+
